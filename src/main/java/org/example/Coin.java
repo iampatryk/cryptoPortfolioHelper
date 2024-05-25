42d63ea0c76bb27dfcff1e1a04fab8e14977f920
iampatryk <mpatrykmiller@gmail.com>
2024-05-25 08:49:46 +0200

    coin list ( added constructors )

--- src/main/java/org/example/Coin.java
+++ src/main/java/org/example/Coin.java
@@ -8,6 +8,30 @@ public class Coin {
     public String symbol;
     public String name;
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
