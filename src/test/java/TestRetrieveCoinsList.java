17f0d7cc8ee9cbe6018a245d65e2a1eb47756416
iampatryk <mpatrykmiller@gmail.com>
2024-05-28 19:34:28 +0200

    coin list ( Tests)

--- src/test/java/TestRetrieveCoinsList.java
+++ src/test/java/TestRetrieveCoinsList.java
@@ -1,7 +1,37 @@
+import org.example.CoinsAPIHandler;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class TestRetrieveCoinsList {
 
+    private CoinsAPIHandler coinsAPIHandler;
+    private Path path;
+    private final String filePathName = "coinsListFile.txt";
 
+
+    @BeforeEach
+    void setUp() {
+        coinsAPIHandler = new CoinsAPIHandler();
+        path = Paths.get(filePathName);
+    }
+
+    @Test
+    void saveResponseToFileShouldUpdateFileWithNewCoins() throws IOException, IOException {
+        String existingCoins = "[{\"id\":\"bitcoin\",\"symbol\":\"btc\",\"name\":\"Bitcoin\"}]";
+        String newCoins = "[{\"id\":\"ethereum\",\"symbol\":\"eth\",\"name\":\"Ethereum\"}]";
+
+        Files.writeString(path,existingCoins);
+
+        coinsAPIHandler.saveResponseToFile(newCoins);
+
+        String updatedCoins = Files.readString(path);
+        Assertions.assertTrue(updatedCoins.contains("etherum"));
+    }
 
 }
